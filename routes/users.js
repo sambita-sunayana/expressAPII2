bf31772a68b4fe534c8bad283b43bf7500fa9e46
sambita-sunayana <samb.sun28@gmail.com>
2024-09-22 21:39:09 +0530

    another commit

--- routes/users.js
+++ routes/users.js
@@ -1,60 +1,4 @@
-import express from 'express';
-import { v4 as uuidv4 } from 'uuid';
-const router = express.Router();
-
-// Mock database
-/*const users = 
- {
-    first_name: 'John',
-    last_name: 'Doe',
-    email: 'johndoe@example.com',
-  },
-  {
-    first_name: 'Alice',
-    last_name: 'Smith',
-    email: 'alicesmith@example.com',
-  },
-];*/
-
-let users = []
-
-// Getting the list of users from the mock database
-router.get('/', (req, res) => {
-    res.send(users);
-})
-
-//post Request AT the home path
-router.post('/', (req, res) => {
-    const user = req.body;
-
-    users.push({ ...user, id: uuidv4() });
-
-    res.send(`${user.first_name} has been added to the Database`);
-})
-//get-get all users
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
-
-  const foundUser = users.find((user) => user.id === id)
-
-  res.send(foundUser)
-});
-
-//delete/;id-delete a spcecific  user based on their id
-router.delete('/:id', (req, res) => {
-  const { id } = req.params;
-
-  users = users.filter((user) => user.id !== id)
-
-  res.send(`${id} deleted successfully from database`);
-});
-//DELETE /;id-delete a spcecific  user based on their id
-router.patch('/:id', (req, res) => {
-  const { id } = req.params;
-
-  const { first_name, last_name, email} = req.body;
-
-  const user = users.find((user) => user.id === id)
+const user = users.find((user) => user.id === id)
 
   if(first_name) user.first_name = first_name;//shift+alt+down
   if(last_name) user.last_name = last_name;
@@ -63,4 +7,6 @@ router.patch('/:id', (req, res) => {
   res.send(`User with the ${id} has been updated`)
 
 });
-export default router
+export default router;
+
+  
